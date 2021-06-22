f7201671361704c563f0297531843000804feeb0
DoaaNegm94 <doaanegm19@gmail.com>
2021-06-22 19:07:59 +0200

    edit@custom.js

--- src/assets/scripts/custom.js
+++ src/assets/scripts/custom.js
@@ -239,3 +239,15 @@ $('input[name="paymentmethod"]').on('click', function () {
 	$('[data-method="' + $value + '"]').slideDown();
 });
 
+// Tap Explore "DoaaNegm"
+
+ $('.menu--item .menu--link').each(function(){
+	 $(this).on('click',function(){
+		 $(this).addClass('active').parent().siblings().find('a').removeClass('active')
+	 });
+ });
+// $('.menu--item').on('click',function(){
+// 	$(this).removeClass('active'); 
+// 	$(this).addClass('active');
+// });
+  
