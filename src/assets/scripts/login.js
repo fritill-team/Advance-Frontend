b1a6d7b6fbfd1e91ed309d2efe5df35fc293ddfd
moossttafa <mostafo7102000@gmail.com>
2021-06-22 16:10:04 +0300

    update layout auth

--- src/assets/scripts/login.js
+++ src/assets/scripts/login.js
@@ -1,10 +1,21 @@
-// function validate() {
-//   const field = document.getElementById('emailtype');
+const field = document.getElementById('emailtype').value;
+//  function validate() {
 //   // CHeck if email
 //       // Validate email address
 //       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field)) {
-//           return (true)
+//           document.getElementsByClassName("key-number").style.value   = "display : none";
+//           document.getElementsByClassName("icon-massege").style.value = "display : block";
+//       }else if(number.test(field)){
+//         console.log("You have entered an number");
+//         return (false)
 //       }
-//       console.log("You have entered an invalid email address!");
-//       return (false)
 //   }
+    // $('input[type=email],select', '#key-number').each(function() {
+    //     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field)) {
+    //         document.getElementsByClassName("key-number").style.value   = "display : none";
+    //         document.getElementsByClassName("icon-massege").style.value = "display : block";
+    //     }else if(number.test(field)){
+    //         console.log("You have entered an number");
+    //         return (false)
+    //     }
+    // })
