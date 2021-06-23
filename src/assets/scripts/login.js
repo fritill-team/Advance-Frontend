3512c1a1bdcbcac092f1cb8886124bf2afc230d9
moossttafa <mostafo7102000@gmail.com>
2021-06-23 11:41:12 +0300

    add a login.js file

--- src/assets/scripts/login.js
+++ src/assets/scripts/login.js
@@ -1,23 +1,16 @@
- //  function validate() {
-//   // CHeck if email
-//       // Validate email address
-//       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field)) {
-//           document.getElementsByClassName("key-number").style.value   = "display : none";
-//           document.getElementsByClassName("icon-massege").style.value = "display : block";
-//       }else if(number.test(field)){
-//         console.log("You have entered an number");
-//         return (false)
-//       }
-//   }
-const field = document.getElementById('emailtype').value;
-    $('input[type=email]', '#key-number').each(function() {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field)) {
-            document.getElementsByClassName("key-number").style.value   = "display : none";
-            document.getElementsByClassName("icon-massege").style.value = "display : block";
-        }else if(number.test(field)){
-            console.log("You have entered an number");
-            return (false)
-        } 
-    })
-    
-    consol.log("dfdfsdffsdfa")
+
+let emailEl = document.querySelector('#emailtype');
+
+emailEl.addEventListener('input', function(e){
+    let value = e.target.value;
+
+    if(value.match(/[\d]+/)){
+        $('#iconmassege').hide();
+        $('#keynumber').show();
+        e.target.type = 'number';
+    } else  {
+        $('#iconmassege').show();
+        $('#keynumber').hide();
+        e.target.type = 'email';
+    }
+})
