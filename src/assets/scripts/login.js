8e845f60494cee005ffffee1f5eb57a1be702f2a
moossttafa <mostafo7102000@gmail.com>
2021-06-23 12:50:26 +0300

    1- remove tag div in all pages pug 2- update webpack packge server

--- src/assets/scripts/login.js
+++ src/assets/scripts/login.js
@@ -4,7 +4,7 @@ let emailEl = document.querySelector('#emailtype');
 emailEl.addEventListener('input', function(e){
     let value = e.target.value;
 
-    if(value.match(/[\d]+/)){
+    if(value.match(/^\d+$/)){
         $('#iconmassege').hide();
         $('#keynumber').show();
         e.target.type = 'number';
