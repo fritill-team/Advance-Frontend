c557b470fae1eb224e04440912cd083bec2b0d7b
MohammedIZaki <zakiz0054@gmail.com>
2021-06-28 16:14:58 +0200

    1- working on input fields

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -6,22 +6,20 @@ import "../styles/vendor/bootstrap/js/bootstrap.bundle.min"
 // require('popper.js');
 // import "../styles/vendor/bootstrap/js/bootstrap.bundle.min.js"
 import "../styles/vendor/OwlCarousel/owl.carousel.js"
-import "../scripts/login"
 // import 'owl.carousel/dist/assets/owl.carousel.css';
 // import 'owl.carousel';
 import "../styles/vendor/semantic/semantic.min.js"
 import "./custom.js"
 import "./night-mode.js"
-import "../scripts/login";
 
-
+import "../scripts/login"
 import "../styles/app.scss";
 import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/js/popper.min.js';
 import Vue from 'vue';
 import Test from "../scripts/vue/Test.vue";
 
-  console.log("hello from webpack");
+console.log("hello from webpack");
 
 new Vue({
   el: '#vue',
