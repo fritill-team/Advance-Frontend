155ed44f7efbc53886a0fabb3979b029d689e8f4
MohammedIZaki <zakiz0054@gmail.com>
2021-07-28 19:05:52 +0200

    1- working on exams

--- src/assets/scripts/vue/Exam.vue
+++ src/assets/scripts/vue/Exam.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1>{{title}}</h1>
+  <div>
+    <h1>{{title}}</h1>
+    <question-wrapper/>
+  </div>
 </template>
 
 <script>
+  import QuestionWrapper from './components/QuestionWrapper.vue' ;
   export default {
     name: 'Exam',
     data() {
       return { title: "test exam page with vue" }
+    },
+    components: {
+      QuestionWrapper
     }
   }
 </script>
