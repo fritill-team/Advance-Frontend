7712a4898a6e4514c2319a83f834908f8d9d5a8c
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-07-28 20:35:19 +0200

    1- fix tabs issue

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -4,7 +4,7 @@ import "./jquery-3.3.1.min.js";
 // import "../styles/vendor/bootstrap/js/bootstrap.bundle.min";
 // require('bootstrap');
 // require('popper.js');
-// import "../styles/vendor/bootstrap/js/bootstrap.bundle.min.js"
+import "../styles/vendor/bootstrap/js/bootstrap.bundle.min.js";
 import "../styles/vendor/OwlCarousel/owl.carousel.js";
 // import 'owl.carousel/dist/assets/owl.carousel.css';
 // import 'owl.carousel';
@@ -12,7 +12,6 @@ import "../styles/vendor/semantic/semantic.min.js";
 import "./custom.js";
 import "./night-mode.js";
 
-// import "../scripts/login";
 import "../styles/app.scss";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -25,6 +24,7 @@ import Vue from "vue";
 import Exam from "../scripts/vue/Exam.vue";
 // import * from 'video';
 
+import "../scripts/login";
 console.log("hello from webpack");
 
 new Vue({
