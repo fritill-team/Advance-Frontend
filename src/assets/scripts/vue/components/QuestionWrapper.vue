b9c3ab6f28154ad13e3d3fab3505b2a54431be98
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-07-28 20:20:36 +0200

    1- edits

--- src/assets/scripts/vue/components/QuestionWrapper.vue
+++ src/assets/scripts/vue/components/QuestionWrapper.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="question-wrapper">
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam recusandae ut, at praesentium reprehenderit soluta eum iste consequatur cupiditate dignissimos natus, dolorem mollitia corporis ratione rem exercitationem itaque fugit.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nulla consectetur modi veritatis tempore, laborum veniam quaerat necessitatibus officia nihil sed sit officiis! Facilis quos commodi, optio dolor perferendis deserunt.</p>
   </div>
 </template>
 
