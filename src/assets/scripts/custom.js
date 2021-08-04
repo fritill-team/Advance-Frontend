a9c8a14bac7dc3b01223f5808a8ba5382146ebaf
mostafa <mostafo7102000@gmail.com>
2021-08-04 18:54:47 +0200

    edite admin layout and create admin dashbord page

--- src/assets/scripts/custom.js
+++ src/assets/scripts/custom.js
@@ -300,11 +300,11 @@ $('.toggle-filter').on('click', function () {
     $('body').addClass('hide')
   }
 })
-$('.list').on('click', function(){
+$('.list').on('click', function () {
   $('.course-card--fixed-width').removeClass('course-card--fixed-width').addClass('course-card--list-view')
   $(this).addClass('active').siblings().removeClass('active')
 })
-$('.grid').on('click', function(){
+$('.grid').on('click', function () {
   $('.course-card--list-view').removeClass('course-card--list-view').addClass('course-card--fixed-width')
   $(this).addClass('active').siblings().removeClass('active')
 
@@ -342,3 +342,44 @@ $('.grid').on('click', function(){
 // var transcriptContainer = document.querySelector('#transcript');
 // transcriptContainer.appendChild(transcript.el());
 
+// sidebar 
+
+'use strict';
+
+var tid = setInterval(function () {
+  if (document.readyState !== 'complete') return;
+  clearInterval(tid);
+  var querySelector = document.querySelector.bind(document);
+  var nav = document.querySelector('.vertical_nav');
+  var wrapper = document.querySelector('.wrapper');
+  var menu = document.getElementById("js-menu");
+  var subnavs = menu.querySelectorAll('.menu--item__has_sub_menu');
+  // Toggle menu click
+  querySelector('.toggle_menu').onclick = function () {
+    nav.classList.toggle('vertical_nav__opened');
+    wrapper.classList.toggle('toggle-content');
+  };
+
+  // Minify menu on menu_minifier click
+  querySelector('.collapse_menu').onclick = function () {
+    nav.classList.toggle('vertical_nav__minify');
+    wrapper.classList.toggle('wrapper__minify');
+    for (var j = 0; j < subnavs.length; j++) {
+      subnavs[j].classList.remove('menu--subitens__opened');
+    }
+  };
+  // Open Sub Menu
+
+  for (var i = 0; i < subnavs.length; i++) {
+    if (subnavs[i].classList.contains('menu--item__has_sub_menu')) {
+      subnavs[i].querySelector('.menu--link').addEventListener('click', function (e) {
+        for (var j = 0; j < subnavs.length; j++) {
+          if (e.target.offsetParent != subnavs[j])
+            subnavs[j].classList.remove('menu--subitens__opened');
+        }
+        e.target.offsetParent.classList.toggle('menu--subitens__opened');
+      }, false);
+
+    }
+  }
+}, 100);
