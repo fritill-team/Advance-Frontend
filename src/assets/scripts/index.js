0f861bed404bfe556aa33529f496f4b9edd075dd
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-08-15 17:24:47 +0200

    1- upgrade vue version

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -22,16 +22,15 @@ import "./lightbox.min.js"
 // import "./videojs-transcript.min.js";
 
 // import 'bootstrap/dist/js/popper.min.js';
-import * as Vue from 'vue';
+import {createApp}from 'vue';
 import Exam from "../scripts/vue/Exam.vue";
+
+createApp(Exam).mount('#vue')
 // import * from 'video';
 
 console.log("hello from webpack");
 
-new Vue({
-  el: "#vue",
-  render: h => h(Exam)
-});
+
 
 
 import "./sortable.js";
