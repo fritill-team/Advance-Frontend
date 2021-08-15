e9a8f2140ac87e26f037552cc129d0660adcc662
mostafa <mostafo7102000@gmail.com>
2021-08-15 12:03:36 +0200

    add function in login

--- src/assets/scripts/login.js
+++ src/assets/scripts/login.js
@@ -94,41 +94,46 @@ var verification = (function () {
 //   console.log("fsdfsd");
 // });
 
- if("intlTelInput" in window){
-  const phoneInputField = document.querySelector("#phone");
+if ("intlTelInput" in window) {
+  const phoneInputField = document.querySelector("#phone"),
+    tokinMask = "5hy8RjDWfiqwO051SlFiQh1hkwDfwvJSSw2my7fF";
+
   const phoneInput = window.intlTelInput(phoneInputField, {
     initialCountry: "eg",
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    preferredCountries: ["eg", "us"],
     utilsScript:
       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
   });
 
 
-const info = document.querySelector(".alert-info");
-const error = document.querySelector(".alert-error");
+  const info = document.querySelector(".alert-info");
+  const error = document.querySelector(".alert-error");
 
-phoneInputField.addEventListener("input" , function process(event) {
-  event.preventDefault();
+  phoneInputField.addEventListener("input", function process(event) {
+    event.preventDefault();
 
-  if(phoneInputField.value.length >= 20){
-    phoneInputField.value = '';
-  }
+    if (phoneInputField.value.length >= 20) {
+      phoneInputField.value = '';
+    }
 
-// IMask(phoneInputField, {
-//   mask: phoneInputField.getAttribute('placeholder').replace(/[0-9]/g, 0)
-// });
+    // IMask(phoneInputField, {
+    //   mask: phoneInputField.getAttribute('placeholder').replace(/[0-9]/g, 0)
+    // });
 
-const phoneNumber = phoneInput.getNumber();
+    const phoneNumber = phoneInput.getNumber();
 
-  info.style.display = "none";
-  error.style.display = "none";
+    info.style.display = "none";
+    error.style.display = "none";
 
-  if (phoneInput.isValidNumber()) {
-    info.style.display = "";
-    info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
-  } else {
-    error.style.display = "";
-    error.innerHTML = `Invalid phone number.`;
-  }
-}) 
+    if (phoneInput.isValidNumber()) {
+      info.style.display = "";
+      info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+    } else {
+      error.style.display = "";
+      error.innerHTML = `Invalid phone number.`;
+    }
+  })
 }
 
