c8334fa6b9b86dcc94ecafb098d1b336a974e1a2
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-08-16 16:41:09 +0200

    1- edits

--- src/vue/components/QuestionWrapper.vue
+++ src/vue/components/QuestionWrapper.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="question-wrapper">
     <div class="question-header">
-      <h3 class="question" v-text="question" />
-      
+      <div class="field-wrapper field-wrapper--no-message mt-0">
+        <label class="text-field__label">Course Title*</label>
+        <div class="text-field__content">
+          <input class="text-field__input" type="text" placeholder="Insert your course title." name="title" data-purpose="edit-course-title" maxlength="" id="main[title1]" value=""/>
+        </div>
+      </div>
+      <select class="question-type form-select form-select-lg" aria-label=".form-select-lg example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
     </div>
-    
+
     <ul class="hints">
       <li v-text="hint" />
     </ul>
