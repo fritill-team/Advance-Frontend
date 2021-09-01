30484546b7c2502665ab841dd8216b337f4978d9
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-09-01 15:27:29 +0200

    1- fixing issues

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 /* src/app.js */
 
 // Styles
-import './assets/styles/app.scss'
-import './assets/styles/base/_fonts.scss'
+import "./assets/styles/app.scss";
+import "./assets/styles/base/_fonts.scss";
 // vendor
-import "./assets/vendor/jquery-3.3.1.min.js"
-import "./assets/vendor/select2.min.js"
+import "./assets/vendor/jquery-3.3.1.min.js";
+import "./assets/vendor/select2.min.js";
 // import "./assets/vendor/fontawesome-free/js/all.js";
 import "./assets/vendor/semantic/semantic.min.js";
 import "./assets/vendor/OwlCarousel/owl.carousel.js";
-// import './assets/vendor/bootstrap/dist/js/popper.min.js';
-import "./assets/vendor/lightbox.min.js"
+
+import "./assets/vendor/bootstrap/js/bootstrap.min.js";
+import "./assets/vendor/popper.min.js";
+import "./assets/vendor/lightbox.min.js";
 import "./assets/vendor/select2.min.js";
 import "./assets/vendor/sortable.js";
 
 // js
-import "./assets/scripts/index"
-
-
+import "./assets/scripts/index";
