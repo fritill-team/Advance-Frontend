4e3ec0744d40758267cc443b4cbada4a11361577
mostafa <mostafo7102000@gmail.com>
2021-09-02 13:26:50 +0200

    edit dropzone in custom js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import "./assets/vendor/OwlCarousel/owl.carousel.js";
 import "./assets/vendor/dropzone.js";
  
 
-import "./assets/vendor/bootstrap/js/bootstrap.min.js";
+// import "./assets/vendor/bootstrap/js/bootstrap.min.js";
 import "./assets/vendor/popper.min.js";
 import "./assets/vendor/lightbox.min.js";
 import "./assets/vendor/select2.min.js";
