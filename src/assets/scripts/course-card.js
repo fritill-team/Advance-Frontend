12a36f07c2439e71c083072a58abd584012ed263
mostafa <mostafo7102000@gmail.com>
2021-09-20 18:07:22 +0200

    course card

--- src/assets/scripts/course-card.js
+++ src/assets/scripts/course-card.js
@@ -1,9 +1,17 @@
 $(".course-hover .card-media__card-img").mouseover(function() {
   $(".course-card" ).addClass("course-card--hover");
-}); 
+});
 $(".course-hover .card-media__card-img").mouseout(function() {
   $(".course-card" ).removeClass("course-card--hover");
-}); 
+});
 // $(".course-hover .card-media__card-img").mousemove(function() {
 //   $(".course-card" ).removeClass("course-card--hover");
-// }); 
+// });
+$(document).ready(function() {
+  $("#iframe").on("mouseover", function(event) {
+    this.play();
+
+  }).on('mouseout', function(event) {
+    this.pause();
+  });
+})
