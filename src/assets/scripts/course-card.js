3b2f6ee46afbe1b734891d48fa8fb287dc18533c
mostafa <mostafo7102000@gmail.com>
2021-09-21 13:03:34 +0200

    course card

--- src/assets/scripts/course-card.js
+++ src/assets/scripts/course-card.js
@@ -1,17 +1,37 @@
-$(".course-hover .card-media__card-img").mouseover(function() {
-  $(".course-card" ).addClass("course-card--hover");
+$(".course-hover .card-media__card-img").mouseover(function () {
+  $(".course-card").addClass("course-card--hover");
 });
-$(".course-hover .card-media__card-img").mouseout(function() {
-  $(".course-card" ).removeClass("course-card--hover");
+$(".course-hover .card-media__card-img").mouseout(function () {
+  $(".course-card").removeClass("course-card--hover");
 });
 // $(".course-hover .card-media__card-img").mousemove(function() {
 //   $(".course-card" ).removeClass("course-card--hover");
 // });
-$(document).ready(function() {
-  $("#iframe").on("mouseover", function(event) {
-    this.play();
+// $(document).ready(function() {
+//   $("#iframe").on("mouseover", function(event) {
+//     $(this).play();
 
-  }).on('mouseout', function(event) {
-    this.pause();
+//   }).on('mouseout', function(event) {
+//     (this).pause();
+//   });
+// })
+// var playersrc = $('#iframe').attr('src');
+// $('#iframe').mouseover(function () {
+//   $('#iframe').attr('src', playersrc + '&autoplay=1');
+// });
+// $('#iframe').mouseout(function () {
+//   $('#iframe').attr('src', playersrc);
+// });
+$(document).ready(function () {
+  $("#iframe").on("mouseover", function () {
+
+    $(this).attr("src", $(this).attr("src") + "&amp;autoplay=1");
   });
-})
+
+  $("#iframe").on("mouseleave", function () {
+    var src = $(this).attr("src");
+    var arr_str = src.split("&amp;");
+    $(this).attr("src", arr_str[0]);
+    console.log(src);
+  });
+});
