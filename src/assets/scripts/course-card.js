786465d3f731711f0d594a301733f44b21077666
sohype.mop@gmail.com <sohype.mop@gmail.com>
2021-09-27 19:04:51 +0200

    fix hover effect

--- src/assets/scripts/course-card.js
+++ src/assets/scripts/course-card.js
@@ -1,9 +1,16 @@
-$(".course-hover .card-media__card-img").mouseover(function() {
-  $(".course-card" ).addClass("course-card--hover");
-}); 
-$(".course-hover .card-media__card-img").mouseout(function() {
-  $(".course-card" ).removeClass("course-card--hover");
-}); 
+// var courseCard = $(".course-hover .card-media__card-img")
+
+$(document)
+  .on("mouseenter", '.course-hover .course-card', function () {
+    $(this).addClass("course-card--hover");
+  })
+  .on("mouseleave", '.course-hover .course-card', function () {
+    $(this).removeClass("course-card--hover");
+  })
+
+  // .mouseout(function () {
+  //   $(".course-card").removeClass("course-card--hover");
+  // });
 // $(".course-hover .card-media__card-img").mousemove(function() {
 //   $(".course-card" ).removeClass("course-card--hover");
-// }); 
+// });
