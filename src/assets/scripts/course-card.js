1bba3710b5f576b5eec38fba5484735790ffa1f1
sohype.mop@gmail.com <sohype.mop@gmail.com>
2021-09-27 23:45:04 +0200

    fixes

--- src/assets/scripts/course-card.js
+++ src/assets/scripts/course-card.js
@@ -11,10 +11,10 @@ $(document)
 const courseTemplate = function (course) {
   return `
     <div class="course-card">
-      <div class="course-card__preview">
-        <div class="course-card__preview-video youtube-player" data-video-id="${course.video_id}"></div>
+      <a href="${course.url}" class="course-card__preview youtube-activator">
+        <div class="course-card__preview-video youtube-player" id="course-${course.id}" data-video-id="${course.video_id}"></div>
         <div class="course-card__preview-overlay"></div>
-      </div>
+      </a>
       <div class="course-card__content">
         <div class="course-card__meta">
           <span class="body-2">${course.subscribers}</span>
@@ -23,11 +23,11 @@ const courseTemplate = function (course) {
             <i class="fa fa-star"></i>${course.total_rate}
           </p>
         </div>
-        <a class="title-link text-2 semi-bold" href="">${course.title}</a>
+        <a class="title-link text-2 semi-bold" href="${course.url}">${course.title}</a>
         <p class="text-3 gray">${course.category}</p>
         <div class="course-card__footer">
            <button class="btn btn--outlined btn--primary">Enroll</button>
-           <button class="btn btn--text btn--danger icon--hover"><i class="fas fa-heart"></i></button>
+           <button class="btn btn--text btn--danger icon--hover"><i class="${course.is_favorite ? 'fas' : 'far'} fa-heart"></i></button>
         </div>
       </div>
     </div>`
