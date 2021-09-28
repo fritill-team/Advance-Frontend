c71aa0a50a0540d324f4c868e270013217043bb5
sohype.mop@gmail.com <sohype.mop@gmail.com>
2021-09-28 17:07:30 +0200

    fix hover on courses

--- src/assets/scripts/course-card.js
+++ src/assets/scripts/course-card.js
@@ -1,11 +1,84 @@
+const breakpoints = {
+  'sm': 576,
+  'md': 768,
+  'lg': 992,
+  'xl': 1200,
+  'xxl': 1400
+}
+const getBreakpoint = function () {
+  var docWidth = $(document).width()
+  if (docWidth < breakpoints.sm)
+    return 'xs'
+  else if (docWidth >= breakpoints.sm && docWidth < breakpoints.md)
+    return 'sm'
+  else if (docWidth >= breakpoints.md && docWidth < breakpoints.lg)
+    return 'md'
+  else if (docWidth >= breakpoints.lg && docWidth < breakpoints.xl)
+    return 'lg'
+  else if (docWidth >= breakpoints.xl && docWidth < breakpoints.xxl)
+    return 'xl'
+  else
+    return 'xxl'
+}
+
 $(document)
   .on("mouseenter", '.course-card-container .course-card__preview', function () {
-    var parent = $(this).closest('.course-card')
+    let parent = $(this).closest('.course-card'),
+      container = $(this).closest('.course-card-container'),
+      count = Number(container.data('count')),
+      col = container.closest("[class^='col-']"),
+      currentBreakpoint = getBreakpoint(),
+      activeClass = col.attr('class')
+        .split(' ')
+        .filter(className => className.indexOf('col-') !== -1)
+        .find(className => className.indexOf(currentBreakpoint) !== -1),
+      colWidth = Number(activeClass.split('-')[2]),
+      noOfColumns = 12 / colWidth,
+      positionNo = count % noOfColumns,
+      position = positionNo !== 0 ? positionNo : noOfColumns,
+      toHideBefore = position - 1,
+      toHideAfter = noOfColumns - position
+    let toHide = col
+    for (let i = 0; i <= toHideAfter; i++) {
+      toHide = toHide.next()
+      toHide.hide()
+    }
+    toHide = col
+    for (let i = 0; i <= toHideBefore; i++) {
+      toHide = toHide.prev()
+      toHide.hide()
+    }
     parent.addClass("course-card--hover")
-
+    col.addClass(`col-${currentBreakpoint}-12`)
   })
   .on("mouseleave", '.course-card-container .course-card__preview', function () {
-    $(this).closest('.course-card').removeClass("course-card--hover")
+    let parent = $(this).closest('.course-card'),
+      container = $(this).closest('.course-card-container'),
+      count = Number(container.data('count')),
+      col = container.closest("[class^='col-']"),
+      currentBreakpoint = getBreakpoint(),
+      activeClass = col.attr('class')
+        .split(' ')
+        .filter(className => className.indexOf('col-') !== -1)
+        .find(className => className.indexOf(currentBreakpoint) !== -1),
+      colWidth = Number(activeClass.split('-')[2]),
+      noOfColumns = 12 / colWidth,
+      positionNo = count % noOfColumns,
+      position = positionNo !== 0 ? positionNo : noOfColumns,
+      toShowBefore = position - 1,
+      toShowAfter = noOfColumns - position;
+    let toShow = col
+    for (let i = 0; i <= toShowAfter; i++) {
+      toShow = toShow.next()
+      toShow.show()
+    }
+    toShow = col
+    for (let i = 0; i <= toShowBefore; i++) {
+      toShow = toShow.prev()
+      toShow.show()
+    }
+    parent.removeClass("course-card--hover")
+    col.removeClass(`col-${currentBreakpoint}-12`)
   })
 
 const courseTemplate = function (course) {
