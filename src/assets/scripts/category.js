978fc5555a34955abc1dd565fa64af678cf500f5
mostafa <mostafo7102000@gmail.com>
2021-10-04 17:06:13 +0200

    edit script in menu

--- src/assets/scripts/category.js
+++ src/assets/scripts/category.js
@@ -1,28 +1,42 @@
+$(document)
+.on('click', '.header-menu', function () {
+  $(".menu").toggleClass('menu--show');
+  $("body").toggleClass("overlay");
+})
+
+
 // open category
-$(document).ready(function(){
-    $(".category").on("click",function(event){
-      $(".category__wrapper").toggleClass("active--category");
-      $("body").toggleClass("overlay"); 
-      event.stopPropagation()
-    }); 
-    $(".wrapper__overlay").on("click",function(){ 
-      $(".category__wrapper").toggleClass("active--category");
-      $("body").toggleClass("overlay"); 
-    }); 
-  }); 
+  // $(".category").on("click",function(event){
+  //   $(".menu").toggleClass("active--menu");
+  //   event.stopPropagation();
+  // }); 
+
+  // $(".wrapper__overlay").on("click",function(event){ 
+  //   $(".menu").toggleClass("active--menu");
+  //   $("body").toggleClass("overlay");
+  //   event.stopPropagation();
+  // }); 
+
+// open side menu 
+
+// $(".sub-menu--item").each(function() {
+// $(".sub-menu--item").on("click",function(){
+//   // $(".menu__content").toggleClass("active-date"); 
+//   $('.menu__date').addClass('active');  
+// }); 
 
 
+// // $(".close").on("click",function(event){
+// //   $(".menu__date").removeClass("active");  
+// //   $(".menu__content").removeClass("active-date");
+// // }); 
+// });
+$(document)
+.on('click', '.sub-menu--item', function () {
+  if ($('.menu__date').hasClass('active')) { 
+    $('.menu__date').removeClass('active');
+  }else { 
+    $('.menu__date').addClass('active');
+  }
+})  
 
-$(".menu__list").each(function() {
-  $(".sub-menu--item").on("click",function(event){
-    $(".category__menu").addClass("active-date"); 
-    $('.category__date').addClass('active');  
-  }); 
-  $(".close").on("click",function(event){
-    $(".category__date").removeClass("active");  
-    $(".category__menu").removeClass("active-date");
-  }); 
-});
-
-
-  
