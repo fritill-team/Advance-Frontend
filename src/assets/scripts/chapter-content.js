cdef5e24c6dd1e119a1be3949099d8ad9c0542f7
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-10-05 13:06:41 +0200

    1- change structure of course content

---
+++ src/assets/scripts/chapter-content.js
@@ -0,0 +1,6 @@
+const chapterTitle = item => `
+<div class="course-list__item"><a class="course-list__header" href="javascript:void(0)">
+<div class="header__header-title"><span class="header-title__title"><i class="uil uil-presentation-play title__icon"></i><span class="title__title-text">Introduction fgdfg to this Course</span></span></div>
+<div class="header__header-details"><span class="header-details__num-item">8 lectures</span><span class="header-details__length-item">22:08</span></div>
+</a></div>
+`
