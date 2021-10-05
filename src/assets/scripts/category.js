b3c4fc55de5de0ad72dbb9df266c66fb9058e70e
mostafa <mostafo7102000@gmail.com>
2021-10-05 11:03:38 +0200

    edit script in menu

--- src/assets/scripts/category.js
+++ src/assets/scripts/category.js
@@ -4,33 +4,14 @@ $(document)
   $("body").toggleClass("overlay");
 })
 
+// close menu on click body
+$(".wrapper__overlay").on("click",function(event){ 
+  $(".menu").toggleClass("menu--show");
+  $("body").toggleClass("overlay");
+  event.stopPropagation();
+}); 
+// open data menu
 
-// open category
-  // $(".category").on("click",function(event){
-  //   $(".menu").toggleClass("active--menu");
-  //   event.stopPropagation();
-  // }); 
-
-  // $(".wrapper__overlay").on("click",function(event){ 
-  //   $(".menu").toggleClass("active--menu");
-  //   $("body").toggleClass("overlay");
-  //   event.stopPropagation();
-  // }); 
-
-// open side menu 
-
-// $(".sub-menu--item").each(function() {
-// $(".sub-menu--item").on("click",function(){
-//   // $(".menu__content").toggleClass("active-date"); 
-//   $('.menu__date').addClass('active');  
-// }); 
-
-
-// // $(".close").on("click",function(event){
-// //   $(".menu__date").removeClass("active");  
-// //   $(".menu__content").removeClass("active-date");
-// // }); 
-// });
 $(document)
 .on('click', '.sub-menu--item', function () {
   if ($('.menu__date').hasClass('active')) { 
@@ -39,4 +20,3 @@ $(document)
     $('.menu__date').addClass('active');
   }
 })  
-
