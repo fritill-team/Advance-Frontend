a03fe8d7c224690dcfbdd7006b1e99f264147554
DoaaNegm94 <doaanegm19@gmail.com>
2021-10-10 12:50:56 +0200

    edit

--- src/assets/scripts/dropdown.js
+++ src/assets/scripts/dropdown.js
@@ -6,27 +6,4 @@ $('.dropdown__activator').each(function () {
     event.stopPropagation();
   })
 })
-
-$('.content__item').each(function () {
-  $(this).on('click', function () {
-    $('.dropdown').removeClass('active')
-  })
-})
-$(document).on('click', function () {
-  $('.dropdown').removeClass('active')
-})
-$(document)
-  .on('click', '.dropdown__activator', function () {
-    var dropdown = $(this).parent('.dropdown')
-    $('.dropdown').removeClass('active')
-    $(this).parent().addClass('active')
-    event.stopPropagation()
-  })
-
-  var myModal = document.getElementById('myModal')
-  var myInput = document.getElementById('myInput')
-  
-  myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
-  })
   
