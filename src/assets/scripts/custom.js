2e274058702d3ebe22c392103093723e59c5a1c7
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-10-11 20:29:02 +0200

    1- working on sidebar

--- src/assets/scripts/custom.js
+++ src/assets/scripts/custom.js
@@ -581,60 +581,68 @@ $(document)
 
 // sidebar
 
-const sidebarTemplate = item => `
-  <li class="list__item ${item.has_children ? 'sub-menu-item': ''}" data-data='${JSON.stringify(item)}'>
+const sidebarTemplate = function (listItem) {
   
-    <a class="item__link" title="Categories">
-      <i class="fas fa-home item__icon"></i>
-      <span class="item__label">${item.title}</span>
-    </a>
-    ${item.hasClass? `
-      ${item.children.map()}
-      <ul class="sub-menu">
-        <li class="sum-menu__item">
-          <a class="sub-menu__link" href="login.html">Login</a>
-        </li>
-        <li class="sum-menu__item">
-          <a class="sub-menu__link" href="signup.html">Sign up </a>
-        </li>
-        <li class="sum-menu__item">
-          <a class="sub-menu__link" href="forget.html">Forget </a>
-        </li>
-        <li class="sum-menu__item">
-          <a class="sub-menu__link" href="reset.html">Reset </a>
-        </li>
-      </ul>
-    `: ''}
-  </li>
-`
+  return `
+    ${listItem.map(item => `
+      <li class="list__item ${item.has_children ? 'sub-menu-item': ''}">
+        <a class="item__link" title="Categories">
+          <i class="fas fa-home item__icon"></i>
+          <span class="item__label">${item.title}</span>
+        </a>
+        ${item.has_children ? `
+          <ul class="sub-menu">
+            <li class="sum-menu__item">
+              <a class="sub-menu__link" href="login.html">Login</a>
+            </li>
+            <li class="sum-menu__item">
+              <a class="sub-menu__link" href="signup.html">Sign up </a>
+            </li>
+            <li class="sum-menu__item">
+              <a class="sub-menu__link" href="forget.html">Forget </a>
+            </li>
+            <li class="sum-menu__item">
+              <a class="sub-menu__link" href="reset.html">Reset </a>
+            </li>
+          </ul>
+        `: ''}
+      </li>
+    `  
+    ).join('')}
+  `
+}
 
-$('.list__sidebar').each((i, listContainer) => {
-  let url = $(listContainer).data('link')
+$('.list__sidebar').each(function(i, item) {
+  let listItem = $(item),
+    data = listItem.data("link")
+  listItem.html($(sidebarTemplate(data)))
+
+})
   
-  $.get(url)
-  .then(res => {
-    // let listItem = $(item),
-    // data = listItem.data("data")
-    // listItem.html($(sidebarTemplate(data)))
-    for (let item of res) {
-      // let course = $(item),
-      // data = course.data("data")
-      // course.html($(sidebarTemplate(data)))
-      $(listContainer).append($(sidebarTemplate(item)))
-      console.log(item);
-      // console.log(item);
-      // let listItem = $(),
-      // data = listItem.data("data")
-      // console.log(data);
-      // listItem.html($(sidebarTemplate(data)))
-    }
-    console.log(listContainer);
+  // $.get(url)
+  // .then(res => {
+  //   // let listItem = $(item),
+  //   // data = listItem.data("data")
+  //   // listItem.html($(sidebarTemplate(data)))
+  //   for (let item of res) {
+  //     // let course = $(item),
+  //     // data = course.data("data")
+  //     // course.html($(sidebarTemplate(data)))
+  //     $(listContainer).append($(sidebarTemplate(item)))
+  //     console.log(item);
+  //     // console.log(item);
+  //     // let listItem = $(),
+  //     // data = listItem.data("data")
+  //     // console.log(data);
+  //     // listItem.html($(sidebarTemplate(data)))
+  //   }
+  //   console.log(listContainer);
     // let listContent = res,
     
     // data = $(item)
     
     // console.log(list);
-  })
-  .catch(e => console.log(e))
-})
+//   })
+//   .catch(e => console.log(e))
+// })
 
