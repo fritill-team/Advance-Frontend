844f4628ccde963657a817b60e8dffe80e698ec5
mostafa <mostafo7102000@gmail.com>
2021-10-12 17:20:52 +0200

    edit menu header

--- src/assets/scripts/menu-header.js
+++ src/assets/scripts/menu-header.js
@@ -1,30 +1,21 @@
 $(document)
 .on('click', '.header-menu', function () {
-  $(".menu").toggleClass('menu--show');
+  $(".menu").toggleClass('menu--open');
   $("body").toggleClass("overlay");
 })
 
 // close menu on click body
 $(".wrapper__overlay").on("click",function(event){ 
-  $(".menu").toggleClass("menu--show");
+  $(".menu").toggleClass("menu--open");
   $("body").toggleClass("overlay");
   event.stopPropagation();
 }); 
 // open data menu
 
-$(document).on('click', '.sub-menu--item', function () {
+$(document).on('click', '.list-item-date', function () {
   if ($('.menu__date').hasClass('active')) { 
     $('.menu__date').removeClass('active');
   }else { 
     $('.menu__date').addClass('active');
   }
 })  
-// $('.sub-menu--item').each(function () { 
-//   $('.sub-menu--item').on("click" , function(){
-//     if ($('.menu__date').hasClass('active')) { 
-//       $('.menu__date').removeClass('active');
-//     }else { 
-//       $('.menu__date').addClass('active');
-//     }
-//   })
-// });
