81e911d9cd172aeec231b02f20e73340b0d9650f
mostafa <mostafo7102000@gmail.com>
2021-10-13 15:18:18 +0200

    dropdown header

--- src/assets/scripts/dropdown.js
+++ src/assets/scripts/dropdown.js
@@ -2,4 +2,7 @@ $('.dropdown__activator').each(function () {
   $(this).on('click', function () { 
     $(this).toggleClass('dropdown--active') 
   })
+  if($(this).siblings().hasClass('dropdown--active')){
+    $(this).siblings().removeClass('dropdown--active')
+  }
 })
