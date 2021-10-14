e733bbb4c84a320de4d0ea4dd89b284d5c9c4287
sohype.mop@gmail.com <sohype.mop@gmail.com>
2021-10-14 14:46:10 +0200

    resource listing

--- src/app.js
+++ src/app.js
@@ -23,8 +23,126 @@ import "./assets/scripts/index";
 import {ListLoader} from "./assets/scripts/backend_scripts/panel-loader"
 
 
+let up = $('.count-up'),
+  down = $('.count-down'),
+  countable = $('.countable')
+
+
+let x = {
+  localCount: 0,
+  get count() {
+    return this.localCount
+  },
+  set count(v) {
+    let value = v
+    if (v >= 5)
+      value = 5
+    if (v <= 0)
+      value = 0
+    this.localCount = value
+    this.countListener(value)
+  },
+  countListener(v) {
+    console.log(v)
+    countable.val(v)
+  }
+}
+
+
+up.on('click', (v) => x.count++)
+down.on('click', (v) => x.count--)
+countable.on('input', v => x.count = v.target.value)
+
+
 export default {
-  ListLoader:function () {
+  ListLoader: function () {
     console.log("lol")
   }
 }
+
+
+class Resource {
+  listingURL = null
+  formURL = null
+  deleteURL = null
+
+  RESOURCE = {
+    // items
+    localItems: [],
+    get items() {
+      return this.localItems
+    },
+    set items(value) {
+      this.localItems = value
+      this.itemsListener(value)
+    },
+    itemsListener(v) {
+      countable.val(v)
+    },
+    // search
+    localSearch: '',
+  }
+
+  constructor($container, options) {
+    this.prefix = options.prefix ? options.prefix : ''
+    this.$container = $container
+    this.listingURL = options.listingURL
+    this.formURL = options.formURL
+    this.deleteURL = options.deleteURL
+    this.itemTemplate = options.itemTemplate
+    this.formTemplate = options.formTemplate
+    this.listingTemplate = options.listingTemplate
+    this.defaultStructure = options.defaultStructure ? options.defaultStructure : this.defaultStructure
+  }
+
+  fetch(Q = null) {
+    $.get(this.listingURL, {q: Q})
+      .then(res => this.RESOURCE.items = res)
+      .catch(e => this.handleError(e))
+
+  }
+
+  handleError(e) {
+    console.error(e)
+  }
+
+  defaultStructure() {
+    return $(`<div class="container">
+        <div class="row">
+        <div class="col-6">
+          ${this.searchForm()}
+          ${this.listingTemplate()}
+        </div>
+        <div class="col-6">${this.formTemplate()}</div>
+        </div>
+    </div>`)
+  }
+
+  async build() {
+    await this.fetch()
+    this.$container.empty()
+    this.$container.append(this.defaultStructure())
+  }
+}
+
+
+let recommendations = new Resource($('#recommendations'), {
+  listingURL: '',
+  formURL: '',
+  deleteURL: '',
+  itemTemplate(item) {
+    return ''
+  },
+  formTemplate(item, url) {
+    return ''
+  },
+  listingTemplate() {
+    return ''
+  },
+  defaultStructure() {
+    return ''
+  }
+})
+
+recommendations.build()
+
