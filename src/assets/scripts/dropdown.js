719595c2268271fb01f38f52f59d978dd014d992
sohype.mop@gmail.com <sohype.mop@gmail.com>
2021-10-19 22:55:18 +0200

    update admin layout

--- src/assets/scripts/dropdown.js
+++ src/assets/scripts/dropdown.js
@@ -1,8 +1,15 @@
-$('.dropdown__activator').each(function () {
-  $(this).on('click', function () { 
-    $(this).toggleClass('dropdown--active') 
+// $('.dropdown__activator').each(function () {
+//   $(this).on('click', function () {
+//     $(this).toggleClass('dropdown--active')
+//   })
+//   if($(this).siblings().hasClass('dropdown--active')){
+//     $(this).siblings().removeClass('dropdown--active')
+//   }
+// })
+
+$(document)
+  .on('click', '.dropdown__activator', function (e) {
+    e.preventDefault()
+    let parent = $(this).closest('.dropdown')
+    parent.addClass('dropdown--active')
   })
-  if($(this).siblings().hasClass('dropdown--active')){
-    $(this).siblings().removeClass('dropdown--active')
-  }
-})
