d19b9ff788f2a0b2ca6d0d74d9356db96b7a77e9
Sohype-Khaled <sohype.mop@gmail.com>
2021-10-27 20:05:13 +0200

    spa layout

--- src/assets/scripts/resource_builder.js
+++ src/assets/scripts/resource_builder.js
@@ -365,3 +365,147 @@ if (classifications) {
   })
   classificationsResource.build()
 }
+
+
+const categories = $('#categories-list')
+if (categories) {
+  let categoriesResource = new ResourceBuilder(categories, {
+    listingURL: categories.data('listing-url'),
+    createURL: categories.data('create-url'),
+    editURL: categories.data('edit-url'),
+    prefix: "categories",
+    itemTemplate(item) {
+      return ` <div class="card" data-data='${JSON.stringify(item)}'>
+      <div class="card__header" >
+        <h4 class='title-5 my-0'>${item.name}</h4>
+        <div class="d-flex card__tools">
+          <div class="dropdown dropdown__activator">
+            <button class="btn btn--info btn--icon btn--text dropdown--btn">
+              <i class="fas fa-ellipsis-h"></i>
+            </button>
+            <div class="dropdown__content">
+                    <div class="list">
+                ${item.actions.map(action => `<a class="list-item list-item--one-line ${action.class}" href="${action.link}">
+                  <div class="list-item__avatar"><i class="${action.icon}">${action.name}</i></div>
+                </a>`)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card__content">
+        <p>${item.description}</p>
+      </div>
+    </div>`
+    },
+    formTemplate(item = {}, action = '') {
+      return `<form action="${action}" method="post">
+      <div class="field-wrapper">
+        <label class="field-wrapper__label" for="categories-name">Name <abbr>*</abbr></label>
+        <div class="field-wrapper__content">
+          <input
+             class="field"
+             type="text"
+             placeholder="Classification"
+             name="name"
+             id="categories-name"
+             required
+             value="${item.name ? item.name : ''}">
+        </div>
+        <ul class="field-wrapper__messages"></ul>
+      </div>
+      <div class="field-wrapper">
+        <label class="field-wrapper__label" for="categories-description">Score <abbr>*</abbr></label>
+        <div class="field-wrapper__content">
+          <input
+            required
+            type="number"
+            class="field"
+            name="score"
+            id="categories-description"
+            placeholder="Classification Score"
+             value="${item.score ? item.score : 0}">
+        </div>
+        <ul class="field-wrapper__messages"></ul>
+      </div>
+      <div class="ml-auto d-inline-block">
+        <button class="btn btn--primary btn--text" type="reset">Cancel</button>
+        <button class="btn btn--primary" type="submit">submit</button>
+      </div>
+    </form>`
+    }
+  })
+  categoriesResource.build()
+}
+
+
+const tags = $('#tags-list')
+if (tags) {
+  let tagsResource = new ResourceBuilder(tags, {
+    listingURL: tags.data('listing-url'),
+    createURL: tags.data('create-url'),
+    editURL: tags.data('edit-url'),
+    prefix: "tags",
+    itemTemplate(item) {
+      return ` <div class="card" data-data='${JSON.stringify(item)}'>
+      <div class="card__header" >
+        <h4 class='title-5 my-0'>${item.name}</h4>
+        <div class="d-flex card__tools">
+          <div class="dropdown dropdown__activator">
+            <button class="btn btn--info btn--icon btn--text dropdown--btn">
+              <i class="fas fa-ellipsis-h"></i>
+            </button>
+            <div class="dropdown__content">
+                    <div class="list">
+                ${item.actions.map(action => `<a class="list-item list-item--one-line ${action.class}" href="${action.link}">
+                  <div class="list-item__avatar"><i class="${action.icon}">${action.name}</i></div>
+                </a>`)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card__content">
+        <p>${item.description}</p>
+      </div>
+    </div>`
+    },
+    formTemplate(item = {}, action = '') {
+      return `<form action="${action}" method="post">
+      <div class="field-wrapper">
+        <label class="field-wrapper__label" for="tags-name">Name <abbr>*</abbr></label>
+        <div class="field-wrapper__content">
+          <input
+             class="field"
+             type="text"
+             placeholder="Classification"
+             name="name"
+             id="tags-name"
+             required
+             value="${item.name ? item.name : ''}">
+        </div>
+        <ul class="field-wrapper__messages"></ul>
+      </div>
+      <div class="field-wrapper">
+        <label class="field-wrapper__label" for="tags-description">Score <abbr>*</abbr></label>
+        <div class="field-wrapper__content">
+          <input
+            required
+            type="number"
+            class="field"
+            name="score"
+            id="tags-description"
+            placeholder="Classification Score"
+             value="${item.score ? item.score : 0}">
+        </div>
+        <ul class="field-wrapper__messages"></ul>
+      </div>
+      <div class="ml-auto d-inline-block">
+        <button class="btn btn--primary btn--text" type="reset">Cancel</button>
+        <button class="btn btn--primary" type="submit">submit</button>
+      </div>
+    </form>`
+    }
+  })
+  tagsResource.build()
+}
