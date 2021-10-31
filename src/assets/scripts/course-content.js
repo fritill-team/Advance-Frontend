a2fcbed2b3d1c708f24c54e9bae1c2da3bf0eb5e
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-10-31 16:47:17 +0200

    1-edits on survey card

--- src/assets/scripts/course-content.js
+++ src/assets/scripts/course-content.js
@@ -1,20 +1,55 @@
 const contentTemplate = (item) => `
-<div class="course-card course-card--grid-view">
-	<a href="${item.url}" class="course-card__preview youtube-activator">
-		<img src="${item.thumb}" alt="" class="course-card__preview-cover">
-		<div class="course-card__preview-overlay"></div>
+${
+  item.course_content
+    ? `
+<div class="content-list-item">
+	<a href="${item.url}" class="content-list-item__image">
+		<img src="${item.thumb}" alt=""/>
 	</a>
-	<div class="course-card__content">
+	<div class="content-list-item__content">
+		<div class="content-list-item__content-header">
+			<h5 class="title-5">
+				${
+          item.enabled
+            ? `<a class="title-5" href="${item.url}">${item.title}</a>`
+            : item.title
+        }
+			</h5>
+			${
+        item.type === "video"
+          ? `<span class="title-5">${item.duration}</span>`
+          : item.type === "exam" && item.enabled
+          ? `<span class="title-5">${item.progress}</span>`
+          : ""
+      }
+		</div>
+		<div class="content-list-item__content-description body-2">
+			${item.description}
+		</div>
 		${
-      item.enabled
-        ? `<a class="title-5" href="${item.url}">${item.title}</a>`
-        : item.title
+      item.type === "video" && item.enabled
+        ? `<div class="content-list-item__progress"><span style= "max-width: ${item.progress}"></span></div>`
+        : ""
     }
-		<p class="title-link course-card__description body-2 gray">${
-      item.description
-    }</p>
-		
 	</div>
+</div>
+`
+    : `
+	<div class="course-card course-card--grid-view">
+		<a href="${item.url}" class="course-card__preview youtube-activator">
+			<img src="${item.thumb}" alt="" class="course-card__preview-cover">
+			<div class="course-card__preview-overlay"></div>
+		</a>
+		<div class="course-card__content">
+			<a class="title-link text-2 semi-bold" href="${item.url}">${item.title}</a>
+			<div class="course-card__meta">
+				<p class="body-2 gray">${item.description}</p>
+			</div>		
+		</div>
+	</div>
+
+`
+}
 
 `;
 
