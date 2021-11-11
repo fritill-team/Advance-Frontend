c93ce58ebed4804f82396327867a3826469de720
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-11-11 21:00:14 +0200

    1- fix layout 2- fix inputs 3- new toggle btn

--- src/assets/scripts/drawer.js
+++ src/assets/scripts/drawer.js
@@ -1,41 +1,48 @@
-const itemTemplate = item => {
-  return `<a href='${item.has_children ? 'javascript:void(0)' : item.url}'
-             class='list-item list-item--one-line${item.is_active ? ' list-item--active' : ''}${item.has_children ? ' list-item--parent' : ''}'>
+const itemTemplate = (item) => {
+  return `<a href='${item.has_children ? "javascript:void(0)" : item.url}'
+             class='list-item list-item--one-line${
+               item.is_active ? " list-item--active" : ""
+             }${item.has_children ? " list-item--parent" : ""}'>
     <div class='list-item__icon'><i class='${item.icon}'></i></div>
     <div class='list-item__content'>
       <p class='body-2'>${item.title}</p>
     </div>
-    ${item.has_children ? `<div class='list-item__action'><i class='fas fa-chevron-down'></i></div>` : ''}
+    ${
+      item.has_children
+        ? `<div class='list-item__action'><i class='fas fa-chevron-down'></i></div>`
+        : ""
+    }
   </a>
-  ${item.has_children ? `<div class='list-item-group'>${item.children.map(child => itemTemplate(child)).join('')}</div>` : ''}`
-}
+  ${
+    item.has_children
+      ? `<div class='list-item-group'>${item.children
+          .map((child) => itemTemplate(child))
+          .join("")}</div>`
+      : ""
+  }`;
+};
 
+const drawerList = (items) => items.map((item) => itemTemplate(item)).join("");
 
-const drawerList = items => items.map(item => itemTemplate(item)).join('')
-
-$('.list__sidebar').each(function(i, item) {
+$(".list__sidebar").each(function (i, item) {
   let listItem = $(item),
-    data = listItem.data('link')
-  listItem.html($(drawerList(data)))
-})
-
+    data = listItem.data("link");
+  listItem.html($(drawerList(data)));
+});
 
 $(document)
-  .on('click', '.toggle-drawer', function() {
-    let target = $(this).data('target'),
-      drawer = $(target)
-
-    if (drawer.hasClass('drawer--open'))
-      drawer.removeClass('drawer--open')
-    else
-      drawer.addClass('drawer--open')
+  .on("click", ".toggle-drawer", function () {
+    let target = $(this).data("target"),
+      drawer = $(target);
+    $(".toggle_animate--icon").toggleClass("toggle-menu--active");
+    if (drawer.hasClass("drawer--open")) drawer.removeClass("drawer--open");
+    else drawer.addClass("drawer--open");
   })
-  .on('click', '.list__sidebar .list-item--parent', function() {
-    let drawer = $(this).closest('.drawer'),
-      group = $(this).next('.list-item-group')
+  .on("click", ".list__sidebar .list-item--parent", function () {
+    let drawer = $(this).closest(".drawer"),
+      group = $(this).next(".list-item-group");
 
-    if (!drawer.hasClass('drawer--open'))
-      drawer.addClass('drawer--open')
+    if (!drawer.hasClass("drawer--open")) drawer.addClass("drawer--open");
 
-    group.css('display', group.css('display') === 'block' ? 'none' : 'block')
-  })
+    group.css("display", group.css("display") === "block" ? "none" : "block");
+  });
--- src/assets/scripts/handleDarkMode.js
+++ src/assets/scripts/handleDarkMode.js
@@ -1,13 +1,39 @@
+const storageInput = document.getElementById("dark-mode-toggle");
+
+// storageInput.addEventListener("change", (val) => {
+// console.log(val.target.checked);
+// localStorage.setItem("darkMode", val.target.checked);
+// const x = localStorage.getItem("darkMode");
+// if (x) {
+//   $("html").addClass("night-mode");
+// } else {
+//   $("html").removeClass("night-mode");
+// }
+// });
+// const x = localStorage.getItem("darkMode");
+
+// storageInput.checked = x;
+
 // localStorage.setItem("darkmode", "off");
-$(document).on("change", "#dark-mode-toggle", function () {
+$(document).on("change", "#dark-mode-toggle", (val) => {
   // if ($("#dark-mode-toggle").is(":checked"))
   //   localStorage.setItem("darkmode", "on");
   // else localStorage.setItem("darkmode", "off");
   // localStorage.getItem("darkmode");
-  if ($("#dark-mode-toggle").is(":checked")) {
-    $("html").addClass("night-mode");
-  } else {
-    $("html").removeClass("night-mode");
-  }
+  // console.log(val.target.checked);
+  // localStorage.setItem("darkMode", val.target.checked);
+  // const dark = localStorage.getItem("darkMode");
+  // val.target.checked = dark;
+  // console.log(dark);
+  // console.log(storageInput.checked);
+  // if (storageInput.checked) {
+  //   $("html").addClass("night-mode");
+  // } else {
+  //   $("html").removeClass("night-mode");
+  // }
   // console.log(localStorage.darkmode);
 });
+
+// const storedValue = localStorage.getItem(darkMode);
+
+// console.log(storedValue);
