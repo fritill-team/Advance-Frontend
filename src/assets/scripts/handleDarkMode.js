fd6277167f0f2d4234a92431589d80970270605a
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-11-30 22:40:26 +0200

    1- edits

--- src/assets/scripts/handleDarkMode.js
+++ src/assets/scripts/handleDarkMode.js
@@ -1,12 +1,58 @@
+
+
+
+let darkMode = localStorage.getItem('darkMode')
+
 const storageInput = document.getElementById("dark-mode-toggle");
-storageInput ? 
+
+
+
+storageInput.checked = darkMode;
+
+const enableDarkMode = () => {
+  $('html').addClass('night-mode')
+  localStorage.setItem('darkMode', true)
+}
+const disableDarkMode = () => {
+  $('html').addClass('night-mode')
+  localStorage.setItem('darkMode', false)
+}
+
+enableDarkMode();
+disableDarkMode();
+// console.log(document.classList());
+// storageInput ? 
+// storageInput.checked = localStorage.getItem('darkMode')
 storageInput.addEventListener("change", (val) => {
-  localStorage.setItem("darkMode", val.target.checked);
-  const x = localStorage.getItem("darkMode");
-  // val.target.checked = x;
-  console.log(val.target.checked, x);
-  x ? $("html").addClass("night-mode") : $("html").removeClass("night-mode");
-}): '';
+  if(val.target.checked){
+    enableDarkMode()
+  }
+  if(!val.target.checked) {
+    disableDarkMode()
+  }
+  // if(darkMode){
+  // } else {
+  // }
+  // localStorage.setItem("darkMode", val.target.checked); val.target.checked);
+  // val.target.checked ? localStorage.setItem("darkMode", true)
+  // : localStorage.setItem("darkMode", false); 
+  
+  // storageInput.checked = localStorage.getItem('darkMode')
+  // console.log(val.target.checked);
+  // val.target.checked = localStorage.getItem("darkMode");
+  // val.target.checked ? console.log('dark') : console.log('light');
+  // const x = localStorage.getItem("darkMode");
+  // // val.target.checked = x;
+  // console.log(val.target.checked, x);
+  // x ? $("html").addClass("night-mode") : $("html").removeClass("night-mode");
+});
+var dark = localStorage.getItem("darkMode");
+// console.log(localStorage.getItem('darkMode'));
+// if (localStorage.getItem('darkMode')) {
+//   $("html").addClass("night-mode")
+// } else {
+//   $("html").removeClass("night-mode")
+// }
 
 // });
 // const x = localStorage.getItem("darkMode");
@@ -15,6 +61,12 @@ storageInput.addEventListener("change", (val) => {
 
 // localStorage.setItem("darkmode", "off");
 $(document).on("change", "#dark-mode-toggle", (val) => {
+  // console.log(dark);
+  // if (localStorage.getItem('darkMode') === true) {
+  //   $("html").addClass("night-mode")
+  // } else {
+  //   $("html").removeClass("night-mode")
+  // }
   // if ($("#dark-mode-toggle").is(":checked"))
   //   localStorage.setItem("darkmode", "on");
   // else localStorage.setItem("darkmode", "off");
--- src/assets/scripts/survey-card.js
+++ src/assets/scripts/survey-card.js
@@ -4,11 +4,16 @@ const surveyTemplate = (item) => `
 		<img src="${item.thumb}" alt="" class="course-card__preview-cover">
 		<div class="course-card__preview-overlay"></div>
 	</a>
+  ${item.sponsor.cover.length > 0 ? `
+    <div class="course-card__sponsor">
+      <img src="${item.sponsor.cover}" >
+    </div>
+  `: ''}
 	<div class="course-card__content">
-		<a class="title-link text-2 semi-bold" href="${item.url}">${item.title}</a>
-		<p class="title-link course-card__description body-2 gray">${item.short_description ? item.short_description : ""}</p>
+		<a class="title-link text-2 semi-bold" href="${item.url}" lang="${item.language}">${item.title}</a>
+		<p class="title-link course-card__description body-2 gray" lang="${item.language}" >${item.short_description ? item.short_description : ""}</p>
 		<div class="course-card__meta">
-			<p class="body-2 gray">${item.category ? item.category : ""}</p>
+			<p class="body-2 gray" lang="${item.language}">${item.category ? item.category : ""}</p>
 		</div>		
 	</div>
 </div>
