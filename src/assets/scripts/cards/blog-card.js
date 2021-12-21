a2e2ccd7306ce853d0b144d27292d2d69a9ab4a7
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-12-21 20:04:23 +0200

    1- edits

--- src/assets/scripts/cards/blog-card.js
+++ src/assets/scripts/cards/blog-card.js
@@ -13,7 +13,7 @@ const blogCard = (item) => `
   </a>
 
   <div class="course-card__content">
-    <img title="${item.author_name}" src="${item.author.image}" class="image image--rounded image--xs-small-profile course-card__author">
+    <img title="${item.author_name}" src="${item.author}" class="image image--rounded image--xs-small-profile course-card__author">
     <div>
       <span class="overline gray">${gettext("published at :")}${item.published_at}</span>
       <a class="title-link text-2 semi-bold" href="${item.url}" lang="${item.language}">${gettext(item.title)}</a>
@@ -42,7 +42,7 @@ $(function(){
       touchDevices: true,
       trigger: 'hover',
       offsetX: '100px',
-      content: $('<div class="user-account"> <img class="image image--profile" src="../../assets/images/hd_dp.jpg" alt=""><div class="user-account__content"> <a class="body-2">John Doe</a><p class="body-2">2 hour ago</p></div></div>')
+      content: $('<div class="user-account"> <img class="image image--profile image--small-profile" src="../../assets/images/hd_dp.jpg" alt=""><div class="user-account__content"> <a class="body-2">John Doe</a><p class="body-2">2 hour ago</p></div></div>')
     });
   })
 })
