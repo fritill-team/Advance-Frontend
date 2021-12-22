7c89d345e2bb2e489df33873268fcc2427bbe940
MohammedIbraheemZaki <zakiz0054@gmail.com>
2021-12-22 18:09:51 +0200

    Updates

--- assets/js/main.bundle.js
+++ assets/js/main.bundle.js
@@ -2884,6 +2884,53 @@ $('.dropdown--header').each(function (item) {
 
 /***/ }),
 
+/***/ "./assets/scripts/handleDarkMode.js":
+/*!******************************************!*\
+  !*** ./assets/scripts/handleDarkMode.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+var darkToggle = document.getElementById("dark-mode-toggle");
+
+if (localStorage.getItem('darkMode') === null) {
+    localStorage.setItem('darkMode', "false");
+}
+
+checkStatus();
+
+function checkStatus() {
+    if (localStorage.getItem('darkMode') === "true") {
+        darkToggle.checked = true; //the checkbox is checked (if you load the page by default it isn’t)
+        $('html').addClass('night-mode'); //the backgornd is set to a dark grey
+    } else {
+        darkToggle.checked = false;
+        $('html').removeClass('night-mode');
+    }
+}
+
+function changeStatus() {
+    //This function gets called every time the checkbox is clicked
+    if (localStorage.getItem('darkMode') === "true") {
+        //if darkMode was active and this function is called it means the user now wants light
+        localStorage.setItem('darkMode', "false"); //so we set it to false, to indicate we are in light mode
+        $('html').removeClass('night-mode');
+    } else {
+        localStorage.setItem('darkMode', "true"); //same code but adapted for dark theme
+        $('html').addClass('night-mode');
+    }
+}
+
+$(document).on("change", "#dark-mode-toggle", function (val) {
+    changeStatus();
+});
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "../node_modules/jquery/dist/jquery.js")))
+
+/***/ }),
+
 /***/ "./assets/scripts/index.js":
 /*!*********************************!*\
   !*** ./assets/scripts/index.js ***!
@@ -2905,6 +2952,8 @@ __webpack_require__(/*! ./night-mode.js */ "./assets/scripts/night-mode.js");
 __webpack_require__(/*! ./login.js */ "./assets/scripts/login.js");
 
 __webpack_require__(/*! ./toaster */ "./assets/scripts/toaster.js");
+
+__webpack_require__(/*! ./handleDarkMode */ "./assets/scripts/handleDarkMode.js");
 
 __webpack_require__(/*! ./tabs.js */ "./assets/scripts/tabs.js");
 
