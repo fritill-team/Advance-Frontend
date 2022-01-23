10212cebc58763c6bce77b476e696ef4d5203453
MohammedIbraheemZaki <zakiz0054@gmail.com>
2022-01-23 11:42:09 +0200

    1- edits

--- src/assets/scripts/select2.js
+++ src/assets/scripts/select2.js
@@ -4,18 +4,21 @@ $(document).ready(function () {
     placeholder: "Select an option",
     allowClear: true
   });
-  function formatData (data) {
-    if (!data.id) { return data.text; }
-    var $result= $(
+
+  function formatData(data) {
+    if (!data.id) {
+      return data.text;
+    }
+    var $result = $(
       '<span><img class="image image--profile" src="../assets/images/get_img.jpg"/> ' + data.text + '</span>'
     );
     return $result;
   };
-  
+
   $("#SelectPeriode").select2({
     templateResult: formatData,
     templateSelection: formatData
-  
+
   });
   // $('.question__type').select2()
 });
